9f0d734bdcb23e63cacc302ec4d183115e9fa970
Adam Elamin <aaelamin0@gmail.com>
2022-12-13 21:48:01 -0500

    Imported the libraries i will use

---
+++ Web_scrapping.py
@@ -0,0 +1,8 @@
+# Import necessary Libraries
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time 
+import pandas as pd
+
